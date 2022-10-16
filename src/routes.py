36219286ef3321f96812870489ec978602db2fe3
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-16 19:08:15 +0300

    Update routes.py

--- src/routes.py
+++ src/routes.py
@@ -1,14 +1,14 @@
-from crypt import methods
-from flask import Flask, render_template, request, redirect, session, flash
-from . import app
-from src.seed import (
-                      add_contact,
-                      get_contact_by_id,
-                      get_contacts,
-                      delete_contact)
+from flask import Flask, render_template, request, redirect, flash, url_for
+from src.seed import add_contact, get_contact_by_id, get_contacts, delete_contact
+from src.__init__ import app
 
 
 @app.route("/", methods=['GET', 'POST'], strict_slashes=False)
+def index():
+    return render_template("base.html")
+
+
+@app.route("/contacts", methods=['GET', 'POST'], strict_slashes=False)
 def contacts():
     if request.method == 'POST':
         first_name = request.form.get('first_name')
@@ -16,24 +16,31 @@ def contacts():
         phone = request.form.get('phone')
         email = request.form.get('email')
         new_contact = add_contact(first_name, last_name, phone, email)
-        flash(f'Contact {new_contact.first_name} {new_contact.last_name} was created')
+        if new_contact:
+            flash(
+                f'Contact {new_contact.first_name} {new_contact.last_name} was created')
+        else:
+            flash('Something went wrong. Try again!')
+    contacts_all = get_contacts()
+    return render_template("pages/contacts/contacts.html", contacts=contacts_all)
 
-    contacts = get_contacts()
-    print(contacts)
-    return render_template("templates/contacts.html", contacts=contacts)
 
-
-@app.route("/detail/<contact_id>", methods=['POST'], strict_slashes=False)
-def detail(contact_id):
+@app.route("/contacts/detail", methods=['POST'], strict_slashes=False)
+def detail():
     if request.method == 'POST':
-        detail = get_contact_by_id(contact_id)
-        return render_template("templates/detail.html", detail=detail)
-    flash(f'Contact {contact_id} does not exist ')
+        contact_id = request.form.get('id')
+        contact = get_contact_by_id(contact_id)
+        if contact:
+            return render_template("pages/contacts/detailContact.html", contact=contact)
+        else:
+            flash(f'Contact ID does not exist ')
+    return redirect(url_for('contacts'))
 
-@app.route("/delete/<contact_id>", methods=['POST'], strict_slashes=False)
+
+@app.route("/contacts/delete/<contact_id>", methods=['POST'], strict_slashes=False)
 def delete(contact_id):
     if request.method == 'POST':
         delete_contact(contact_id)
         flash('Contact was deleted')
-        return redirect("/")
-    return redirect("/")
+    return redirect(url_for('contacts'))
+
