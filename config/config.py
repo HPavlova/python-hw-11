5a39061c9d4d31acda24722cbb9c9d1a7854684c
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-17 20:49:30 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,8 +4,8 @@ from dotenv import dotenv_values
 BASE_DIR = pathlib.Path(__file__).parent.parent
 config = dotenv_values(".env")
 
+
 class Config:
-    UPLOAD_FOLDER = str(BASE_DIR / "uploads")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / 'data' / 'contacts.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / 'data' / 'contacts.db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = config['SECRET_KEY']
-    
